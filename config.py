16ea734ac3a6f473fc17a52cf43fe7e80374e192
Andrew Wei <andrewscwei@gmail.com>
2019-04-27 18:39:00 -0700

    Add categories

--- config.py
+++ config.py
@@ -1,54 +1,13 @@
 import re
+from typing import Dict, List
 
-max_col_widths = {
+max_col_widths: Dict[str, int] = {
   "date": 6,
   "code": 23,
   "desc": 40,
   "value": 15,
 }
 
-exclude_line_regexes = [
+exclude_line_regexes: List[str] = [
   r'PAYMENT - THANK YOU',
 ]
-
-categories = {
-  "Alcohol": [],
-  "Transportation/Gas": [],
-  "Transportation/Insurance": [],
-  "Transportation/Loans": [],
-  "Transportation/Maintenance": [],
-  "Transportation/Parking": [],
-  "Transportation/Penalties": [],
-  "Bank/Charges": [
-    r''
-  ],
-  "Bank/Interests": [],
-  "Business/Admin": [],
-  "Coffee": [],
-  "Clothing": [],
-  "Commute/Public Transit": [],
-  "Digital/Recreation": [],
-  "Digital/Resources": [],
-  "Digital/Utilities": [],
-  "Electronics/Accessories": [],
-  "Electronics/Devices": [],
-  "Electronics/Recreation": [],
-  "Events/Business": [],
-  "Events/Recreation": [],
-  "Food": [],
-  "Groceries": [],
-  "Hobbies": [],
-  "Household/Bills": [],
-  "Household/Enhancements": [],
-  "Income": [],
-  "Investments/Cryptocurrency": [],
-  "Investments/RRSP": [],
-  "Medical/Drugs": [],
-  "Medical/Insurance": [],
-  "Medical/Practitioners": [],
-  "Office Supplies": [],
-  "Telecom/Home": [],
-  "Telecom/Mobile": [],
-  "Travel": [],
-  "Others": [],
-}
--- parse.py
+++ parse.py
@@ -2,7 +2,8 @@ import re
 import sys
 from typing import Match
 
-from config import categories, exclude_line_regexes, max_col_widths
+from categories import categories
+from config import exclude_line_regexes, max_col_widths
 
 
 # Prettifies all lines in the statement, adding uniform spacing between columns.
@@ -35,7 +36,7 @@ file.close()
 # Count original number of lines, for output reference only.
 old_cloc: int = len(read_str.split('\n'))
 
-# Remove all blank lines but leave one at the EOF.
+# Remove all blank lines but leave one at EOF for easier parsing later on.
 read_str = re.sub(r'\n(?!({0}))'.format(regex_date), ' ', read_str) + '\n'
 
 # Remove posting date that is before the transaction date and only keep
@@ -46,7 +47,7 @@ read_str = re.sub(r'({0}) ({1})'.format(regex_date, regex_date), r'\1', read_str
 read_str = re.sub(r'(Foreign Currency.*)({0})'.format(regex_value), r'\2', read_str)
 
 # Move transaction code to the end.
-read_str = re.sub(r'({0})(.*)({1})(.*)'.format(regex_date, regex_code), r'\1 \2 \4 \3', read_str)
+read_str = re.sub(r'({0})(.*)({1})(.*)'.format(regex_date, regex_code), r'\1 \2 \4', read_str)
 
 # Remove tabs.
 read_str = re.sub(r'\t', ' ', read_str)
@@ -57,6 +58,11 @@ read_str = re.sub(r' +', ' ', read_str)
 # Remove all lines in the regexes of lines to exclude.
 for regex in exclude_line_regexes:
   read_str = re.sub(re.compile('%s%s%s' % (r'(.*?)', regex, r'(.*?)\n')), '', read_str)
+
+# Assign transactions to known categories.
+for category in categories:
+  for regex in categories[category]:
+    read_str = re.sub(re.compile('%s%s%s' % (r'(.*?', regex, r'.*?)\n')), r'\1' + ' ' + category + r' \n', read_str)
 
 # Apply space formatting.
 read_str = re.sub(r'({0}) +(.*) +({1})'.format(regex_date, regex_value), format_statement, read_str)
@@ -69,8 +75,9 @@ file = open(file_out, 'w')
 file.write(read_str)
 file.close()
 
-# Count total lines in output string.
-new_cloc = len(read_str.split('\n'))
+# Count total lines in output string and minus one to account for the blank line
+# at EOF.
+new_cloc = len(read_str.split('\n')) - 1
 
 print(read_str)
 print()
